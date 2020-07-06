9449f2959c94d0c66b55d06a62371c5cce6a841b
GuzelNazargulova <65545319+GuzelNazargulova@users.noreply.github.com>
2020-07-06 19:33:00 +0500

    Add files via upload

---
+++ Case.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <valarray>
+#include <string>
+using namespace std;
+using vec = valarray<double>;
+
+class Case
+{
+public:
+	int NumPoint;
+	double Lx;
+	double dt;
+	double h;
+	double NuDivHH;
+	double HiDivHH;
+	double param;
+	vec T;
+	vec dT;
+	vec v;
+	vec dv;
+
+	Case(int NumPoint, double L, double Nu, double Hi, double dP, double r);
+	void SetInitial(vec T, vec v);
+	void step();
+	void write(string adr);
+};
